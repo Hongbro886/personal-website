10221c4d721b10d32bc7b9e96cc658340ecd3337
Hongbro886 <hongbroyier@gmail.com>
2026-06-18 14:03:26 +0800

    feat: 完善并微调

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -75,6 +75,8 @@ const Home = () => {
             <li>喜欢地理这门有趣的学科</li>
             <li>常玩的游戏：Minecraft，Palworld</li>
             <li>品味过以下神作：Outer Wilds，双人成行，黑神话：悟空</li>
+            <li>随机现充刷新在各种有趣活动现场</li>
+            <li>SECTL 人事组、网站组成员，SECTL Community 群管理员</li>
             <li>欢迎交流、合作，以及来玩</li>
           </ul>
         </div>
